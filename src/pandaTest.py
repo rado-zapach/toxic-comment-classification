9ac81e5a460f7908d3123b8612e3bab6a1c8d92d
DESKTOP-LPNU653\Rado <r.zapach@gmail.com>
2018-03-31 21:52:07 +0200

    stop words, lemmatization, sentiment

--- src/pandaTest.py
+++ src/pandaTest.py
@@ -1,30 +1,48 @@
+import nltk
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.feature_extraction import text
+from textblob import TextBlob
+import time
+
+start = time.time()
 
 # load data from csv
 df = pd.read_csv('../data/train.csv')
 
 # new column - joins all toxic columns as one boolean column
-df['t'] = (df['toxic'] + df['severe_toxic'] + df['obscene'] + df['threat'] + df['insult'] + df['identity_hate']) > 0
+df['toxicity'] = (df['toxic'] + df['severe_toxic'] + df['obscene'] + df['threat'] + df['insult'] + df['identity_hate']) > 0
 # remove unnecessary columns
 del df['id'], df['toxic'], df['severe_toxic'], df['obscene'], df['threat'], df['insult'], df['identity_hate']
 
 # new column - number of words in text
 df['words'] = df['comment_text'].str.split().str.len()
-# new column - number uppercase letters in text
+
+# new column - number of uppercase letters in text
 df['upper'] = df['comment_text'].str.count('[A-Z]')
 
-# df[df.t].groupby("words").size()
-# df[df.words == 1250]
+# sentiment
+df[['polarity', 'subjectivity']] = df['comment_text'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
+print("sentiment done")
 
-# df2 = df[df.t]
-# del df2['comment_text'], df2['t']
+# remove stop words
+stop = text.ENGLISH_STOP_WORDS
+pat = r'\b(?:{})\b'.format('|'.join(stop))
+df['without_stopwords'] = df['comment_text'].str.replace(pat, '')
+df['without_stopwords'] = df['without_stopwords'].str.replace(r'\s+', ' ')
+print("stop words done")
 
-# df2.groupby('words').size().plot()
-# df2.groupby('upper').size().plot()
-# plt.show()
+# lemmatization
+nltk.download('wordnet')
+w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
+lemmatizer = nltk.stem.WordNetLemmatizer()
 
-# df2.plot()
-# df2.plot(x='words', y='upper', kind='scatter')
-# plt.show()
+
+def lemmatize_text(text):
+    return " ".join([lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(text)])
+
+
+df['lemmatized'] = df.without_stopwords.apply(lemmatize_text)
+print("lemmatization done")
+
+end = time.time()
+print(end - start)
