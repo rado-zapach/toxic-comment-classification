8899b159881931e7cf02257b2b7309cc03db95c8
Radoslav Zapach <r.zapach@gmail.com>
2018-04-06 13:19:45 +0200

    added word and upper case normalization, link detection

--- src/pandaTest.py
+++ src/pandaTest.py
@@ -4,6 +4,24 @@ from sklearn.feature_extraction import text
 from textblob import TextBlob
 import time
 
+def lemmatize_text(text):
+    return " ".join([lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(text)])
+
+def toxicity_to_int(toxicity):
+    if toxicity:
+        return 1
+    else:
+        return 0
+
+def check_links(text):
+    if re.search("(?P<url>https?://[^\s]+)", text) is not None:
+        return 1
+    else:
+        return 0
+
+def uppercase_normalize(text):
+    return sum(1 for c in text if c.isupper()) / len(text)
+
 start = time.time()
 
 # load data from csv
@@ -11,14 +29,23 @@ df = pd.read_csv('../data/train.csv')
 
 # new column - joins all toxic columns as one boolean column
 df['toxicity'] = (df['toxic'] + df['severe_toxic'] + df['obscene'] + df['threat'] + df['insult'] + df['identity_hate']) > 0
+
 # remove unnecessary columns
 del df['id'], df['toxic'], df['severe_toxic'], df['obscene'], df['threat'], df['insult'], df['identity_hate']
 
+# change boolean toxicity to 1/0
+df['toxic'] = df.toxicity.apply(toxicity_to_int)
+del df['toxicity']
+
 # new column - number of words in text
 df['words'] = df['comment_text'].str.split().str.len()
+# normalize words count
+max_words = df['words'].max()
+df['words_normalized'] = df['words'] / max_words
+del df['words']
 
-# new column - number of uppercase letters in text
-df['upper'] = df['comment_text'].str.count('[A-Z]')
+# normalize upper case count
+df['upper_normalized'] = df.comment_text.apply(uppercase_normalize)
 
 # sentiment
 df[['polarity', 'subjectivity']] = df['comment_text'].apply(lambda Text: pd.Series(TextBlob(Text).sentiment))
@@ -36,13 +63,11 @@ nltk.download('wordnet')
 w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
-
-def lemmatize_text(text):
-    return " ".join([lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(text)])
-
-
 df['lemmatized'] = df.without_stopwords.apply(lemmatize_text)
 print("lemmatization done")
+
+# check if comment has any link
+df['has_link'] = df.comment_text.apply(check_links)
 
 end = time.time()
 print(end - start)
