543fb7e9936f96fd534cdef34401e111cbc852c9
DESKTOP-LPNU653\Rado <r.zapach@gmail.com>
2018-05-12 21:40:06 +0200

    added working tfidf

--- src/training_tests/multiple_trainings.py
+++ src/training_tests/multiple_trainings.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from sklearn.linear_model import RidgeClassifier
 from sklearn.linear_model import SGDClassifier
@@ -19,9 +20,9 @@ def do_classification(clf, X_train, X_test, y_train, y_test):
     y_pred = clf.predict(X_test.values)
 
     accuracy = accuracy_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred, average="macro")
-    precision = precision_score(y_test, y_pred, average="macro")
-    recall = recall_score(y_test, y_pred, average="macro")
+    f1 = f1_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
 
     print('accuracy, f1, precision, recall')
     print(str(round(accuracy, 2)))
@@ -31,46 +32,43 @@ def do_classification(clf, X_train, X_test, y_train, y_test):
     print()
 
 
-df = pd.read_csv("../data/transformed.csv", usecols=["polarity","subjectivity","has_link","upper_normalized","words_normalized","toxic"])
+df = pd.read_csv("../../data/transformed.csv")
+
+# retrieve tfidf frequencies
+vectoriser = TfidfVectorizer(min_df=0.01, max_df=0.7)
+# put tfidf to new dataframe for easier work
+df2 = pd.DataFrame(list(vectoriser.fit_transform(df['lemmatized'].values.astype('U')).toarray()))
+# add tfidf attributes to existing attributes
+df = pd.concat([df, df2], axis=1)
+
+# remove unnecesery columns
+del df['comment_text']
+del df['without_stopwords']
+del df['lemmatized']
+# column count - all columns from col. 2 are attributes
+# cols 7 and more are tfidf
+columnCount = len(df.columns)
+
+# print column names (numbers are for tf/idf)
+print(list(df))
 
 toxic = df.loc[df['toxic'] == 1]
 nontoxic = df.loc[df['toxic'] == 0]
 nontoxic = nontoxic.sample(n=len(toxic.index))
 df = pd.concat([toxic, nontoxic])
 
-X_train, X_test, y_train, y_test = train_test_split(df[["polarity","subjectivity","has_link","upper_normalized","words_normalized"]], df["toxic"], test_size=0.2)
+X_train_t, X_test_t, y_train_t, y_test_t = train_test_split(toxic.iloc[:, 2:columnCount], toxic["toxic"], test_size=0.2)
+X_train_n, X_test_n, y_train_n, y_test_n = train_test_split(nontoxic.iloc[:, 2:columnCount], nontoxic["toxic"], test_size=0.2)
+X_train = X_train_t.append(X_train_n)
+X_test = X_test_t.append(X_test_n)
+y_train = y_train_t.append(y_train_n)
+y_test = y_test_t.append(y_test_n)
 
-# X_train, X_test, y_train, y_test = train_test_split(df[["polarity"]], df["toxic"], test_size=0.2)
-# X_train, X_test, y_train, y_test = train_test_split(df[["subjectivity"]], df["toxic"], test_size=0.2)
-# X_train, X_test, y_train, y_test = train_test_split(df[["has_link"]], df["toxic"], test_size=0.2)
-# X_train, X_test, y_train, y_test = train_test_split(df[["upper_normalized"]], df["toxic"], test_size=0.2)
-# X_train, X_test, y_train, y_test = train_test_split(df[["words_normalized"]], df["toxic"], test_size=0.2)
-
-X_train, X_test, y_train, y_test = train_test_split(df[["has_link", "upper_normalized", "words_normalized"]], df["toxic"], test_size=0.2)
-
-clf = svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-                  decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-                  max_iter=-1, probability=False, random_state=None, shrinking=True,
-                  tol=0.001, verbose=False)
+clf = svm.SVC(C=1.0, cache_size=200, gamma='auto', kernel='rbf').fit(X_train, y_train)
 do_classification(clf, X_train, X_test, y_train, y_test)
 
-# clf = RidgeClassifier(tol=1e-2, solver="lsqr")
-# do_classification(clf, X_train, X_test, y_train, y_test)
-#
-# clf = KNeighborsClassifier(n_neighbors=10)
-# do_classification(clf, X_train, X_test, y_train, y_test)
-#
 # clf = RandomForestClassifier(n_estimators=100)
 # do_classification(clf, X_train, X_test, y_train, y_test)
-#
-# clf = SGDClassifier(alpha=.0001, n_iter=50, penalty="elasticnet")
-# do_classification(clf, X_train, X_test, y_train, y_test)
-#
-# clf = NearestCentroid()
-# do_classification(clf, X_train, X_test, y_train, y_test)
-#
-# clf = MultinomialNB(alpha=.01)
-# do_classification(clf, X_train, X_test, y_train, y_test)
-#
+
 # clf = DecisionTreeClassifier(max_depth=20)
 # do_classification(clf, X_train, X_test, y_train, y_test)
--- src/training_tests/train_decision_tree.py
+++ src/training_tests/train_decision_tree.py
@@ -8,12 +8,21 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 
-df = pd.read_csv("../../data/transformed2.csv")
+df = pd.read_csv("../../data/transformed.csv")
 
-vectoriser = TfidfVectorizer(min_df=0.2)
-df['tfidf'] = list(vectoriser.fit_transform(df['lemmatized'].values.astype('U')).toarray())
+vectoriser = TfidfVectorizer(min_df=0.01, max_df=0.7)
+df2 = pd.DataFrame(list(vectoriser.fit_transform(df['lemmatized'].values.astype('U')).toarray()))
+df = pd.concat([df, df2], axis=1)
 
-# print(df.tfidf)
+del df['comment_text']
+del df['without_stopwords']
+del df['lemmatized']
+columnCount = len(df.columns)
+
+# print column names (numbers are for tf/idf)
+print(list(df))
+
+# print(df)
 
 # ngram_vectorizer = CountVectorizer(ngram_range=(1, 1), token_pattern=r'\b\w+\b', min_df=1)
 # df['ngrams'] = list(ngram_vectorizer.fit_transform(df['lemmatized'].values.astype('U')).toarray())
@@ -23,38 +32,20 @@ df['tfidf'] = list(vectoriser.fit_transform(df['lemmatized'].values.astype('U'))
 toxic = df.loc[df['toxic'] == 1]
 nontoxic = df.loc[df['toxic'] == 0]
 nontoxic = nontoxic.sample(n=len(toxic.index))
-#df = pd.concat([toxic, nontoxic])
 
-
-X_train_t, X_test_t, y_train_t, y_test_t = train_test_split(toxic['tfidf'], toxic["toxic"], test_size=0.2)
-X_train_n, X_test_n, y_train_n, y_test_n = train_test_split(nontoxic['tfidf'], nontoxic["toxic"], test_size=0.2)
+X_train_t, X_test_t, y_train_t, y_test_t = train_test_split(toxic.iloc[:, 2:columnCount], toxic["toxic"], test_size=0.2)
+X_train_n, X_test_n, y_train_n, y_test_n = train_test_split(nontoxic.iloc[:, 2:columnCount], nontoxic["toxic"], test_size=0.2)
 X_train = X_train_t.append(X_train_n)
 X_test = X_test_t.append(X_test_n)
 y_train = y_train_t.append(y_train_n)
 y_test = y_test_t.append(y_test_n)
 
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
-
-X_train = X_train.tolist()
-X_test = X_test.tolist()
-y_train = y_train.tolist()
-y_test = y_test.tolist()
+# print(X_train.shape, y_train.shape)
+# print(X_test.shape, y_test.shape)
 
 clf = svm.SVC(C=1.0, cache_size=200, gamma='auto', kernel='rbf').fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
-
-# skore = 0
-# for j in range(0,len(test_vals)):
-#     if(test_vals[j] == y_test[j]):
-#         skore = skore + 1
-#
-# print(skore)
-# print(len(test_vals))
-# print(skore/len(test_vals))
-
-# print(df.tfidf)
 
 print(f1_score(y_test, y_pred))
 print(precision_score(y_test, y_pred))
